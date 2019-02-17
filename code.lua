03c3f7863b0d97e9e8234362fd9939ccd46b2bb3
dogmax <walus91@gmail.com>
2019-02-17 18:25:42 +0100

    Initial Upload

---
+++ code.lua
@@ -0,0 +1,8 @@
+local FRAME = CreateFrame("FRAME")
+
+TargetofTargetFrame:SetScript("OnShow", 
+	function(self)
+		TargetofTargetFrame:ClearAllPoints();
+		TargetofTargetFrame:SetPoint("RIGHT", "TargetFrame", "BOTTOMRIGHT", 0, 5);
+	end 
+)
